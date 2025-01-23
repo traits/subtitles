22a78eedb0356a96b60b13a19817104d7d65dd90
Michael Bieber <projects@traits.de>
2025-01-24 00:10:38 +0100

    Fixed pytorch installation; stopped package creation by uv (still creating a redundant *egg-info directory); site.txt removed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+# import torch
+
 from processor import Processor
 
 if __name__ == "__main__":
+    # if torch.cuda.is_available():
+    #     print("CUDA ON")
+    # else:
+    #     print("CUDA OFF")
+
     processor = Processor()
     processor.run()
