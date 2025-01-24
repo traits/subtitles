afe54747a4979f5ef5250e55c5edd82067749e52
Michael Bieber <projects@traits.de>
2025-01-24 18:27:46 +0100

    create_venv.ps1 simplified

--- _python/create_venv.ps1
+++ _python/create_venv.ps1
@@ -1,18 +1,6 @@
-param()
-
-$ErrorActionPreference = "Stop"
 Push-Location $PSScriptRoot
 
-$VENV_DIR = "../.venv"
-$VENV_PYTHON = Join-Path $VENV_DIR "Scripts/python.exe"
-
-# Create environment with locked Python version
-uv venv --python 3.12 $VENV_DIR
-
-# Install the project and its dependencies using uv with preview mode
-#$env:UV_PREVIEW=1
-# uv pip install -e . --python $VENV_PYTHON
-uv sync --project "../"
+uv sync --project "../" --link-mode=copy
 
 Write-Host "Environment built from pyproject.toml"
 Pop-Location
