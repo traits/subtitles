d9e450f7916f569d42963fb0e11270b26bc6edbb
Michael Bieber <projects@traits.de>
2025-01-24 10:45:03 +0100

    Use uv sync to get rid of egg-info directory creation

--- _python/create_venv.ps1
+++ _python/create_venv.ps1
@@ -10,8 +10,9 @@ $VENV_PYTHON = Join-Path $VENV_DIR "Scripts/python.exe"
 uv venv --python 3.12 $VENV_DIR
 
 # Install the project and its dependencies using uv with preview mode
-$env:UV_PREVIEW=1
-uv pip install -e . --python $VENV_PYTHON
+#$env:UV_PREVIEW=1
+# uv pip install -e . --python $VENV_PYTHON
+uv sync --project "../"
 
 Write-Host "Environment built from pyproject.toml"
 Pop-Location
