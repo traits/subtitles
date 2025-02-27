234e92411fda409fbc6541fa48ab7d57bd13127a
Michael Bieber <projects@traits.de>
2025-02-27 14:51:51 +0100

    PyTorch upgrade; Qwen/Qwen2.5-VL-7B-Instruct added; Code for switching between models, fast_attn tested (not successful)

--- _python/create_venv.ps1
+++ _python/create_venv.ps1
@@ -1,6 +1,6 @@
 Push-Location $PSScriptRoot
 
-uv sync --project "../" --link-mode=copy
+uv sync --project "../" --link-mode=copy --upgrade
 
 Write-Host "Environment built from pyproject.toml"
 Pop-Location
